2329f7cc7a34be2dd9dabeb31a9e43aaf59075a7
AKrendyasov <akrendyasov@gmail.com>
2019-10-02 15:55:21 +0300

    2 files modified

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element:{
     type:string,
     name:string,
@@ -13,7 +13,12 @@ export class ServerElementComponent implements OnInit {
   }
   constructor() { }
 
+  ngOnChanges () {
+    console.log("ngOnChanges")
+  }
+
   ngOnInit() {
   }
+
 
 }
